aea61acf2fdbfb50fd985d44a503efbd5f81fd7b
Peter Gale <peter.gale@sophos.com>
2019-12-01 21:45:11 -0800

    Cleanup day 2 part two based on cleanup from day 2 part one.

--- 2019/day/2/part2/aoc.pl
+++ 2019/day/2/part2/aoc.pl
@@ -3,42 +3,50 @@
 use strict;
 use warnings;
 
+my %opcodes = (
+    1 => sub {
+        my ($prog, $offset) = @_;
+        $prog->[$prog->[$offset+3]] = $prog->[$prog->[$offset+1]] + $prog->[$prog->[$offset+2]];
+        return 4;
+    },
+    2 => sub {
+        my ($prog, $offset) = @_;
+        $prog->[$prog->[$offset+3]] = $prog->[$prog->[$offset+1]] * $prog->[$prog->[$offset+2]];
+        return 4;
+    },
+    99 => sub {
+        my ($prog, $offset) = @_;
+        return 0;
+    }
+);
+
 my $input = <STDIN>;
 chomp $input;
 
+
 for my $noun (0..99) {
     for my $verb (0..99) {
-
         my @a = split /,/, $input;
-
         my $i = 0;
 
         $a[1] = $noun;
         $a[2] = $verb;
 
         while (1) {
-            my $opcode = $a[$i];
-            if ($opcode == 1) {
-                $a[$a[$i+3]] = $a[$a[$i+1]] + $a[$a[$i+2]];
-            }
-            elsif ($opcode == 2) {
-                $a[$a[$i+3]] = $a[$a[$i+1]] * $a[$a[$i+2]];
-            }
-            elsif ($opcode == 99) {
-                last;
+            if (ref $opcodes{$a[$i]} eq 'CODE') {
+                my $res = $opcodes{$a[$i]}->(\@a, $i);
+                last unless $res;
+                $i += $res;
             }
             else {
-                die "Unknown opcode: $opcode";
+                die "Unknown opcode: $a[$i] at offset=$i";
             }
-
-            $i += 4;
         }
 
         if ($a[0] == 19690720) {
-            print 100 * $noun + $verb;
-            last;
+            printf "%d\n", 100 * $noun + $verb;
+            exit;
         }
     }
 }
 
-print "\n";
