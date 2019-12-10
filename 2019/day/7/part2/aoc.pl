e9cbb08029f74a290ac77e05e873c200fc6528a5
Peter Gale <peter.gale@sophos.com>
2019-12-10 13:16:44 -0800

    Add callbacks to IntCode so that I can remove all the special handling of different in_fh/out_fh.
    - Supported callbacks are: read_cb, write_cb, halt_db, and pre_run_cb.

--- 2019/day/7/part2/aoc.pl
+++ 2019/day/7/part2/aoc.pl
@@ -5,10 +5,85 @@ use warnings;
 
 package IntCode;
 
-my $final_out_fh = *STDOUT;
+my %opcodes = (
+    1 => sub {
+        my ($self) = @_;
+        $self->set_value(3, $self->get_value(1) + $self->get_value(2));
+        $self->{ip} += 4;
+    },
+    2 => sub {
+        my ($self) = @_;
+        $self->set_value(3, $self->get_value(1) * $self->get_value(2));
+        $self->{ip} += 4;
+    },
+    3 => sub {
+        my ($self) = @_;
+        my $input;
+        if (ref($self->{read_cb}) eq 'CODE') {
+            $input = $self->{read_cb}->($self);
+        }
+        else {
+            $input = <STDIN>;
+            chomp $input;
+        }
+        $self->set_value(1, $input);
+        $self->{ip} += 2;
+    },
+    4 => sub {
+        my ($self) = @_;
+        my $value = $self->get_value(1);
+        if (ref($self->{write_cb}) eq 'CODE') {
+            $self->{write_cb}->($self, $value);
+        }
+        else {
+            print "$value\n";
+        }
+        $self->{ip} += 2;
+    },
+    5 => sub {
+        my ($self) = @_;
+        if ($self->get_value(1)) {
+            $self->{ip} = $self->get_value(2);
+        }
+        else {
+            $self->{ip} += 3;
+        }
+    },
+    6 => sub {
+        my ($self) = @_;
+        if ($self->get_value(1) == 0) {
+            $self->{ip} = $self->get_value(2);
+        }
+        else {
+            $self->{ip} += 3;
+        }
+    },
+    7 => sub {
+        my ($self) = @_;
+        $self->set_value(3, $self->get_value(1) < $self->get_value(2) ? 1 : 0);
+        $self->{ip} += 4;
+    },
+    8 => sub {
+        my ($self) = @_;
+        $self->set_value(3, $self->get_value(1) == $self->get_value(2) ? 1 : 0);
+        $self->{ip} += 4;
+    },
+    9 => sub {
+        my ($self) = @_;
+        $self->{relative_base} += $self->get_value(1);
+        $self->{ip} += 2;
+    },
+    99 => sub {
+        my ($self) = @_;
+        if (ref($self->{halt_cb}) eq 'CODE') {
+            $self->{halt_cb}->($self);
+        }
+        return -1;
+    }
+);
 
 sub new {
-    my ($class, $prog_file, $phase) = @_;
+    my ($class, $prog_file) = @_;
     die unless $class && $prog_file;
 
     open my $fh, "<$prog_file" or die "Failed to open $prog_file for read: $!";
@@ -18,93 +93,53 @@ sub new {
 
     return bless {
         prog => [ split /,/, $prog ],
-        phase => $phase,
         ip => 0,
         relative_base => 0,
-        opcodes => {
-            1 => sub {
-                my ($self) = @_;
-                $self->set_value(3, $self->get_value(1) + $self->get_value(2));
-                $self->{ip} += 4;
-            },
-            2 => sub {
-                my ($self) = @_;
-                $self->set_value(3, $self->get_value(1) * $self->get_value(2));
-                $self->{ip} += 4;
-            },
-            3 => sub {
-                my ($self) = @_;
-                my $in_fh = $self->{in_fh} // *STDIN;
-                my $input = <$in_fh>;
-                chomp $input;
-                $self->set_value(1, $input);
-                $self->{ip} += 2;
-            },
-            4 => sub {
-                my ($self) = @_;
-                my $out_fh = $self->{out_fh} // *STDOUT;
-                my $value = $self->get_value(1);
-                local $SIG{PIPE} = sub {
-                    close $out_fh;
-                    print $final_out_fh "$value\n";
-                    close $final_out_fh;
-                };
-                print $out_fh "$value\n";
-                $self->{ip} += 2;
-            },
-            5 => sub {
-                my ($self) = @_;
-                if ($self->get_value(1)) {
-                    $self->{ip} = $self->get_value(2);
-                }
-                else {
-                    $self->{ip} += 3;
-                }
-            },
-            6 => sub {
-                my ($self) = @_;
-                if ($self->get_value(1) == 0) {
-                    $self->{ip} = $self->get_value(2);
-                }
-                else {
-                    $self->{ip} += 3;
-                }
-            },
-            7 => sub {
-                my ($self) = @_;
-                $self->set_value(3, $self->get_value(1) < $self->get_value(2) ? 1 : 0);
-                $self->{ip} += 4;
-            },
-            8 => sub {
-                my ($self) = @_;
-                $self->set_value(3, $self->get_value(1) == $self->get_value(2) ? 1 : 0);
-                $self->{ip} += 4;
-            },
-            9 => sub {
-                my ($self) = @_;
-                $self->{relative_base} += $self->get_value(1);
-                $self->{ip} += 2;
-            },
-            99 => sub {
-                return -1;
-            }
-        },
     }, $class;
 }
 
-sub set_in_fh {
-    my ($self, $in_fh) = @_;
-    $self->{in_fh} = $in_fh;
+sub set_read_cb {
+    my ($self, $cb) = @_;
+
+    if ($cb) {
+        $self->{read_cb} = $cb;
+    }
+    else {
+        delete $self->{read_cb};
+    }
 }
 
-sub set_out_fh {
-    my ($self, $out_fh) = @_;
-    $self->{out_fh} = $out_fh;
+sub set_write_cb {
+    my ($self, $cb) = @_;
+
+    if ($cb) {
+        $self->{write_cb} = $cb;
+    }
+    else {
+        delete $self->{write_cb};
+    }
 }
 
-sub set_final_out_fh {
-    my ($self, $out_fh) = @_;
-    $final_out_fh = $out_fh;
+sub set_halt_cb {
+    my ($self, $cb) = @_;
+
+    if ($cb) {
+        $self->{halt_cb} = $cb;
+    }
+    else {
+        delete $self->{halt_cb};
+    }
+}
+
+sub set_pre_run_cb {
+    my ($self, $cb) = @_;
+
+    if ($cb) {
+        $self->{pre_run_cb} = $cb;
+    }
+    else {
+        delete $self->{pre_run_cb};
+    }
 }
 
 sub set_value {
@@ -143,16 +178,14 @@ sub get_value {
 sub run {
     my $self = shift;
 
-    my $out_fh = $self->{out_fh} // *STDOUT;
-    print $out_fh "$self->{phase}\n" if defined $self->{phase};
+    if (ref($self->{pre_run_cb}) eq 'CODE') {
+        $self->{pre_run_cb}->($self);
+    }
 
     while (1) {
         my $res = $self->opcode();
         last if $res == -1;
     }
-
-    close $self->{in_fh} if defined $self->{in_fh};
-    close $self->{out_fh} if defined $self->{out_fh};
 }
 
 sub opcode {
@@ -160,8 +193,8 @@ sub opcode {
 
     my $ip = $self->{ip};
     my $opcode = $self->{prog}->[$ip] % 100;
-    if (ref $self->{opcodes}{$opcode} eq 'CODE') {
-        return $self->{opcodes}{$opcode}->($self);
+    if (ref $opcodes{$opcode} eq 'CODE') {
+        return $opcodes{$opcode}->($self);
     }
     else {
         die "Unknown opcode: $opcode ($self->{prog}->[$ip]) at offset=$ip";
@@ -227,15 +260,44 @@ sub calc_output_signal {
             close $readers[$_] for grep { $_ != $i } (0..$#readers);
             close $writers[$_] for grep { $_ != $i+1 && $_ != $w_i } (0..$#writers);
 
-            my $ic = IntCode->new($prog_file, $i < $#seq ? $seq[$i+1] : ());
-            $ic->set_in_fh($readers[$i]);
-            if ($i == $#seq) {
-                $ic->set_out_fh($writers[0]);
-                $ic->set_final_out_fh($writers[$i+1]);
+            my $ic = IntCode->new($prog_file);
+            if ($i < $#seq) {
+                $ic->set_pre_run_cb(
+                    sub {
+                        print {$writers[$i+1]} "$seq[$i+1]\n";
+                    });
             }
-            else {
-                $ic->set_out_fh($writers[$i+1]);
-            }
+
+            $ic->set_read_cb(
+                sub {
+                    my $in_fh = $readers[$i];
+                    chomp(my $in = <$in_fh>);
+                    return $in;
+                });
+
+            $ic->set_write_cb(
+                sub {
+                    my ($self, $value) = @_;
+                    if ($i == $#seq) {
+                        local $SIG{PIPE} = sub {
+                            close $writers[0];
+                            print {$writers[$i+1]} "$value\n";
+                            close $writers[$i+1];
+                        };
+                        print {$writers[0]} "$value\n";
+                    }
+                    else {
+                        print {$writers[$i+1]} "$value\n";
+                    }
+                });
+
+            $ic->set_halt_cb(
+                sub {
+                    my ($self) = @_;
+                    close $readers[$i];
+                    close $writers[$i+1];
+                });
+
             $ic->run();
 
             exit;
