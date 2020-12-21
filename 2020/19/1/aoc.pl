b74666ea869e0be9e6f6de38ea363b9ac81ff46e
Peter Gale <peter.gale@sophos.com>
2020-12-21 00:14:28 -0800

    Add solutions for 2020 day 19 parts 1 and 2.

---
+++ 2020/19/1/aoc.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my %rules;
+while (my $line = <>) {
+    chomp $line;
+    last if $line eq '';
+    if ($line =~ m/^(\d+): (.*)$/) {
+        $rules{$1} = $2;
+    }
+    else {
+        die "Unexpected rule: $line";
+    }
+}
+
+my $regex = build_re(0);
+
+sub build_re {
+    my ($id) = @_;
+
+    my $re = '';
+    if ($rules{$id} =~ m/^"([a-z])"$/) {
+        $re .= $1;
+    }
+    elsif ($rules{$id} =~ m/^([0-9 ]+)$/) {
+        for my $i (split / /, $1) {
+            $re .= build_re($i);
+        }
+    }
+    elsif ($rules{$id} =~ m/^([0-9 ]+) \| ([0-9 ]+)$/) {
+        my ($a, $b) = ($1, $2);
+        $re .= '(?:';
+        for my $i (split / /, $a) {
+            $re .= build_re($i);
+        }
+        $re .= '|';
+        for my $i (split / /, $b) {
+            $re .= build_re($i);
+        }
+        $re .= ')';
+    }
+    else {
+        die "Unexpected rule: $rules{$id}";
+    }
+
+    return $re;
+}
+
+my $match = 0;
+while (my $line = <>) {
+    chomp $line;
+    $match++ if $line =~ qr/^$regex$/;
+}
+
+print $match . "\n";
