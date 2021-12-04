df4d4a6520cb0979017a95a89a1a26f0728e5d0b
Peter Gale <peter.gale@sophos.com>
2021-12-03 22:35:46 -0800

    Add solution for 2021 day 4 part 1.

---
+++ 2021/4/1/aoc.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::Util qw/sum/;
+
+my @nums = split /,/, <>;
+<>; # read blank line
+
+my @boards;
+my @board;
+while (my $line = <>) {
+    chomp $line;
+    if ($line eq '') {
+        push @boards, [ @board ];
+        @board = ();
+        next;
+    }
+    $line =~ s/^ *//;
+    $line =~ s/ *$//;
+    push @board, [ split / +/, $line ];
+}
+push @boards, [ @board ];
+
+for my $num (@nums) {
+    draw_num($num, \@boards);
+
+    for my $board (@boards) {
+        my $res = check_board($board, $num);
+        if ($res != -1) {
+            print $res . "\n";
+            exit 0;
+        }
+    }
+}
+
+sub draw_num {
+    my ($n, $boards) = @_;
+    for my $board (@$boards) {
+        @$board = map { my $i = $_; [ map { $board->[$i][$_] != $n ? $board->[$i][$_] : -1 } (0..4) ] } (0..4);
+    }
+}
+
+sub check_board {
+    my ($board, $num) = @_;
+
+    for my $i (0..4) {
+        if (scalar(grep { $board->[$i][$_] < 0 } (0..4)) == 5 || scalar(grep { $board->[$_][$i] < 0 } (0..4)) == 5) {
+            return $num * sum(map { my $j = $_; sum(map { $board->[$j][$_] > 0 ? $board->[$j][$_] : 0 } (0..4)) } (0..4));
+        }       
+    }
+    
+    return -1;
+}
