2523aa1dcfc82b2faeae120d27a8df9c8641044c
Peter Gale <peter.gale@sophos.com>
2021-12-12 22:20:49 -0800

    Add solution for 2021 day 13.

---
+++ 2021/13/1/aoc.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::Util qw/max sum/;
+
+my @map;
+my $max_x = 0;
+my $max_y = 0;
+while (my $line = <>) {
+    chomp $line;
+    if ($line =~ m/^(\d+),(\d+)$/) {
+        $map[$2][$1] = 1;
+        $max_x = max($max_x, $1);
+        $max_y = max($max_y, $2);
+    }
+    elsif ($line =~ m/^fold along ([xy])=(\d+)$/) {
+        if ($1 eq 'x') {
+            @map = map { my $y = $_; [ map { ($map[$y][$_] // 0) | ($map[$y][$max_x-$_] // 0) } (0..$2-1) ] } (0..$max_x);
+            $max_x = $2-1;
+        }
+        elsif ($1 eq 'y') {
+            @map = map { my $y = $_; [ map { ($map[$y][$_] // 0) | ($map[$max_y-$y][$_] // 0) } (0..$max_x) ] } (0..$2-1);
+            $max_y = $2-1;
+        }
+        last;
+    }
+    elsif ($line ne '') {
+        warn "Unexpected line: $line";
+    }
+}
+
+print sum(map { my $y = $_; map { $map[$y][$_] } (0..$max_x) } (0..$max_y)) . "\n";
