a2780fe5d0212dce82276ab3148a9d333f6e345c
Peter Gale <peter.gale@sophos.com>
2021-12-15 22:57:15 -0800

    Add solution for 2021 day 16.

---
+++ 2021/16/1/aoc.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use bigint;
+
+use List::Util qw/sum/;
+
+my @c = map { chomp; map { split //, sprintf("%04b", hex($_)) } split // } <>;
+
+my @pkt_func = map { \&operator } (0..7); $pkt_func[4] = \&literal;
+
+print packet(\@c) . "\n";
+
+sub parse_bits {
+    my ($c, $bits) = @_;
+    my $res = '0b';
+    $res .= shift @$c foreach (1..$bits);
+    return oct($res);
+}
+
+sub packet {
+    my $c = shift;
+    my $version = parse_bits($c, 3);
+    my $type = parse_bits($c, 3);
+    return $version + $pkt_func[$type]->($c, $type);
+}
+
+sub literal {
+    my $c = shift;
+    my $res = '0b';
+    while (@$c) {
+        my $more = shift @$c;
+        $res <<= 4;
+        $res += parse_bits($c, 4);
+        return 0 unless $more;
+    }
+}
+
+sub operator {
+    my $c = shift;
+    my $length_type_id = shift @$c;
+    my @versions;
+    if ($length_type_id) {
+        @versions = map { packet($c) } (1..parse_bits($c, 11));
+    }
+    else {
+        my $rem = abs(parse_bits($c, 15) - scalar(@$c));
+        while (scalar(@$c) > $rem) {
+            push @versions, packet($c);
+        }
+    }
+
+    return sum(@versions);
+}
---
+++ 2021/16/2/aoc.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use bigint;
+
+use List::Util qw/sum product min max/;
+
+my @c = map { chomp; map { split //, sprintf("%04b", hex($_)) } split // } <>;
+
+my @pkt_func = map { \&operator } (0..7); $pkt_func[4] = \&literal;
+my @op_func = (\&sum, \&product, \&min, \&max, undef, sub { $_[0] > $_[1] ? 1 : 0 }, sub { $_[0] < $_[1] ? 1 : 0 }, sub { $_[0] == $_[1] ? 1 : 0 });
+
+print packet(\@c) . "\n";
+
+sub parse_bits {
+    my ($c, $bits) = @_;
+    my $res = '0b';
+    $res .= shift @$c foreach (1..$bits);
+    return oct($res);
+}
+
+sub packet { 
+    my $c = shift;
+    my $version = parse_bits($c, 3);
+    my $type = parse_bits($c, 3);
+    return $pkt_func[$type]->($c, $type);
+}
+
+sub literal {
+    my ($c, $type) = @_;
+    my $res = 0;
+    while (@$c) {
+        my $more = shift @$c;
+        $res <<= 4;
+        $res += parse_bits($c, 4);
+        return $res unless $more;
+    }
+}
+
+sub operator {
+    my ($c, $type) = @_;
+    my $length_type_id = shift @$c;
+    my @operands;
+    if ($length_type_id) {
+        @operands = map { packet($c) } (1..parse_bits($c, 11));
+    }
+    else {
+        my $rem = abs(parse_bits($c, 15) - scalar(@$c));
+        while (scalar(@$c) > $rem) {
+            push @operands, packet($c);
+        }
+    }
+
+    return $op_func[$type]->(@operands);
+}
