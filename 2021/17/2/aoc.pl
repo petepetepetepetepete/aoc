0823a00c57f50f398a7632b131f7052d860d730c
Peter Gale <peter.gale@sophos.com>
2021-12-18 13:32:32 -0800

    Add solution for 2021 day 17.

---
+++ 2021/17/2/aoc.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::Util qw/min max/;
+
+my ($min_x,$max_x,$min_y,$max_y) = <> =~ m/^target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)/;
+
+print scalar(map { my $y = $_; map { height($_,$y) } (-$max_x..$max_x) } ($min_y..abs($min_y))) . "\n";
+
+sub height {
+    my ($dx, $dy) = @_;
+    my ($x,$y) = (0) x 2;
+    my $res = $y;
+    while ($dx != 0 || $dy >= 0 || $y >= $min_y) {
+        $res = max($res, $y);
+        return $res if $x >= $min_x && $x <= $max_x && $y >= $min_y && $y <= $max_y;
+        $x += $dx;
+        $y += $dy;
+        $dx-- if $dx > 0;
+        $dx++ if $dx < 0;
+        $dy--;
+    }
+    return;
+}
