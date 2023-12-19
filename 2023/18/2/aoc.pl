3717a404aa60684db3bdbfc736661948e848f386
Peter Gale <peter.gale@sophos.com>
2023-12-19 00:24:44 -0800

    Add solution for 2023 day 18 part 2.

---
+++ 2023/18/2/aoc.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Math::Polygon;
+
+my @dir = qw/R D L U/;
+my @m = (['#']);
+my @plan = map { chomp; [ split / /, $_ ] } <>;
+
+for my $p (@plan) {
+    my ($a, $b) = $p->[2] =~ m/^\(#(.{5})(.)\)$/;
+    $p->[0] = $dir[$b];
+    $p->[1] = hex($a);
+}
+
+my ($x,$y) = (0,0);
+
+my @points = [$x,$y];
+for my $i (0..$#plan) {
+    my $step = $plan[$i];
+    my ($dir, $len, $rgb) = @$step;
+
+    my $s = join('', map { my $j = ($i+$_) % scalar(@plan); $plan[$j][0] } (-1..1));
+    if ($s =~ m/^(?:RDL|DLU|LUR|URD)$/) {
+        $len++;
+    }
+    elsif ($s =~ m/^(?:RUL|DRU|LDR|ULD)$/) {
+        $len--;
+    }
+
+    if ($dir eq 'R') {
+        $x += $len;
+    }
+    elsif ($dir eq 'L') {
+        $x -= $len;
+    }
+    elsif ($dir eq 'U') {
+        $y += $len;
+    }
+    elsif ($dir eq 'D') {
+        $y -= $len;
+    }
+    else {
+        die "Unexpected dir: $dir";
+    }
+    push @points, [$x,$y];
+}
+
+my $poly = Math::Polygon->new(@points);
+print $poly->area . "\n";
