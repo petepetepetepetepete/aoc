c31b2d7162b2ee67490e13d2d5028ef625615869
Peter Gale <peter.gale@sophos.com>
2023-12-19 02:30:54 -0800

    Add solution for 2023 day 17 part 1.
    - use dijkstra for finding shortest path

--- 2023/17/1/aoc.pl
+++ 2023/17/1/aoc.pl
@@ -3,7 +3,43 @@
 use strict;
 use warnings;
 
-use List::Util qw/all min sum/;
+package Node;
+
+use overload
+    '==' => \&equals,
+    '""' => \&to_string;
+
+sub new {
+    my ($class, $x, $y, $hl) = @_;
+    return bless { x => $x, y => $y, heatloss => $hl, neighbours => {} }, $class;
+} 
+
+sub x { $_[0]->{x} }
+sub y { $_[0]->{y} }
+sub heatloss { $_[0]->{heatloss} }
+sub neighbours { $_[0]->{neighbours} }
+
+sub add_neighbour {
+    my ($self, $dir, $node) = @_;
+    $self->{neighbours}{$dir} = $node;
+}
+
+sub equals {
+    my ($a, $b) = @_;
+    return $a->x == $b->x && $a->y == $b->y;
+}
+
+sub to_string {
+    my ($self) = @_;
+    return sprintf("%d,%d", $self->x, $self->y);
+}
+
+sub distance {
+    my ($a, $b) = @_;
+    return abs($a->x - $b->x) + abs($a->y - $b->y);
+}
+
+1;
 
 my %opp = (
     '>' => '<',
@@ -22,58 +58,60 @@ my @m = map { chomp; [ split //, $_ ] } <>;
 my $max_y = $#m;
 my $max_x = $#{$m[0]};
 
-my @steps = map { [ [ $_ ], 0 ] } get_next(0, 0);
+my @node_map = map { my $y=$_; [ map { Node->new($_, $y, $m[$y][$_]) } (0..$max_x) ] } (0..$max_y);
+for my $y (0..$max_y) {
+    for my $x (0..$max_x) {
+        my @cur = ($x,$y);
+        while (my ($dir, $pt) = each %dir) {
+            my ($nx,$ny) = map { $cur[$_] + $pt->[$_] } (0..$#cur);
+            next if $nx < 0 or $nx > $max_x or $ny < 0 or $ny > $max_y;
 
-my @paths;
-my @directions;
-my @hl;
-my %cache;
-while (my $step = shift @steps) {
-    my ($path, $hl) = @$step;
-    my @path = @$path;
-    my $prev = $path[-1];
-
-    my ($x, $y, $dir) = @$prev;
-
-    my $hl2 = $hl + $m[$y][$x];
-    if ($x == $max_x && $y == $max_y) {
-        push @paths, [ $path, $hl2 ];
-        next;
-    }
-
-    for my $next (get_next($x, $y, $path)) {
-        my $k = join(',', @$prev,@$next);
-        next if (defined $cache{$k} && $cache{$k} < $hl2);
-
-        $cache{$k} = $hl2;
-        push @steps, [
-            [ @$path, $next ],
-            $hl2
-        ];
+            $node_map[$y][$x]->add_neighbour($dir, $node_map[$ny][$nx]);
+        }
     }
 }
 
-my @spaths = sort { $a->[1] <=> $b->[1] } @paths;
-print $spaths[0][1] . "\n";
+my $start = $node_map[0][0];
+my $target = $node_map[$max_y][$max_x];
 
-sub get_next {
-    my ($x, $y, $prev) = @_;
-    $prev //= [];
+# x,y,dir,dircount
+my %seen;
 
-    my @next;
-    for my $d (qw/> < ^ v/) {
-        my $nx = $x + $dir{$d}[0];
-        my $ny = $y + $dir{$d}[1];
-        next if $nx < 0 or $nx > $max_x or $ny < 0 or $ny > $max_y;
+# [ node, cost, last 3 dir ]
+my @q = [ $start, 0, [] ];
+while (@q) {
+    my ($node, $cost, $dirs) = @{shift @q};
+    my @dirs = @$dirs;
 
-        # no backtracking
-        next if scalar(@$prev) >= 2 && $prev->[-2][0] == $nx && $prev->[-2][1] == $ny;
-
-        # can't move in the same direction for more than three blocks
-        next if scalar(@$prev) >= 3 && all { $_->[2] eq $d } @{$prev}[-3..-1];
-
-        push @next, [ $nx, $ny, $d ];
+    # we've reached our goal
+    if ($node == $target) {
+        print $cost . "\n";
+        last;
     }
 
-    return @next;
+    # count how many times we've move in the same direction
+    my $dir_count = 0;
+    my $i = -1;
+    my $dir = $dirs->[$i] // '';
+    $dir_count++ while defined $dirs->[$i] && $dir eq $dirs->[$i--];
+
+    my $k = join(',', $node->x, $node->y, $dir, $dir_count);
+
+    # skip if we've come to this node the same way before
+    next if $seen{$k};
+
+    $seen{$k}++;
+
+    while (my ($ndir, $neighbour) = each %{$node->neighbours()}) {
+        # skip if this next step would cause us to go too far in the same direction
+        next if $dir eq $ndir && $dir_count == 3;
+
+        # skip if this next step would cause us to backtrack
+        next if $opp{$ndir} eq $dir;
+
+        push @q, [ $neighbour, $cost + $neighbour->heatloss, [ @$dirs, $ndir ] ];
+    }
+
+    # sort by cost
+    @q = sort { $a->[1] <=> $b->[1] } @q;
 }
